dd9fac19ae695c3db528acbee849146c13bf4ead
JcKDanie1s <99392811+JcKDanie1s@users.noreply.github.com>
2022-03-07 01:54:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,55 +4,42 @@
 
 
 double pown(double value, uint16_t n) {
-  double first = value;
-  for (int i = 1; i < n; i++) {
-    value = value * first;
-  }
-  return value;
+if (n == 1) {
+        return value;
+    }
+    return value * pown(value, n - 1);
 }
 
 uint64_t fact(uint16_t n) {
-  uint64_t res;
-  if (n == 0) {
-    res = 1;
-    return res;
-  }
-  res = fact(n - 1) * n;
-  return res;
+ if (n == 0) {
+        return 1;
+    }
+    return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-  return pown(x, n) / fact(n);
+return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  double sum = 1;
-  for (count; count > 0; count--) {
-    sum = calcItem(x, count) + sum;
-  }
-  return sum;
+if (count == 0) {
+        return 1;
+    }
+    return calcItem(x, count) + expn(x, count - 1);
 }
 
 double expn(double x, uint16_t count) {
-  double sum = x;
-  int step = 3;
-  int minus = -1;
-  for (int i = 2; i <= count; i++) {
-    sum += minus * calcItem(x, step);
-    step += 2;
-    minus *= -1;
-  }
-  return sum;
+if (count == 1) {
+        return x;
+    }
+    return calcItem(x, 2 * count - 1) * pown(-1, count - 1) +
+    sinn(x, count - 1);
 }
 
 double sinn(double x, uint16_t count) {
-double sum = 1;
-  int step = 2;
-  int minus = -1;
-  for (int i = 2; i <= count; i++) {
-    sum += minus * calcItem(x, step);
-    step += 2;
-    minus *= -1;
-  }
-  return sum;
+ if (count == 1) {
+        return 1;
+    }
+    return calcItem(x, 2 * count - 2) * pown(-1, count - 1) +
+    cosn(x, count - 1);
 }
