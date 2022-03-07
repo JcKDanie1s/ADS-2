8d5fc1effd1d41c3d6c9b1b03c0c2add7353fd62
JcKDanie1s <99392811+JcKDanie1s@users.noreply.github.com>
2022-03-07 23:37:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,47 +2,47 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-if (n == 1) {
-return value;
-} else if (n == 0) {
-return 1;
-}
-return value * pown(value, --n);
+uint64_t i;
+double t = 1;
+for (i = 1; i <= n; i++)
+t *= value;
+return t;
 }
 
 uint64_t fact(uint16_t n) {
-if (n <= 1) {
+if (n == 0)
 return 1;
-}
-return n * fact(n - 1);
-}
-
-double calcItem(double x, uint16_t n) {
-return pown(x, n) / fact(n);
+return fact(n-1)*n;
 }
 
-double expn(double x, uint16_t count) {
-double sum = 0;
-for (uint16_t i = 0, n = 0; i <= count; ++i, ++n) {
-sum += calcItem(x, n);
-}
-return sum;
+double calcItem(double t, uint16_t n) {
+return pown(t, n)/fact(n);
 }
 
-double expn(double x, uint16_t count) {
-double sum = 0;
-for (uint16_t i = 1, n = 1; i <= count; ++i, ++n) {
-sum += pown(-1, n - 1) * calcItem(x, 2 * n - 1);
+double expn(double t, uint16_t count) {
+uint16_t i;
+double a;
+for (i = 0; i <= count; i++) {
+a += calcItem(t, i);
 }
-return sum;
+return a;
 }
 
-double sinn(double x, uint16_t count) {
-double sum = 0;
-for (uint16_t i = 1, n = 1; i <= count; ++i, ++n) {
-sum += pown(-1, n - 1) * calcItem(x, 2 * n - 2);
+double sinn(double t, uint16_t count) {
+uint16_t i;
+double a = 0;
+for (i = 1; i <= count; i++) {
+a += pown(-1, i-1)*calcItem(t, 2*i-1);
 }
-return sum;
+return a;
+}
+
+double cosn(double t, uint16_t count) {
+uint16_t i;
+double a = 0;
+for (i = 1; i <= count; i++) {
+a += pown(-1, i-1)*calcItem(t, 2*i-2);
+}
+return a;
 }
